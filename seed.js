85e5b7377e87a054e301fba4c4308e00d7abf56f
Mehran Hatami <mehranhatamie@gmail.com>
2016-01-06 12:55:22 +0330

    remove emails

--- seed.js
+++ seed.js
@@ -3,44 +3,44 @@ import './api/users/user.model';
 const User = mongoose.model('User');
 
 const seed = {
-  data : [{
-    email: 'mhatami@thinkful.com',
+  data: [{
+    email: 'user1@example.com',
     firstName: 'Mehran',
     lastName: 'Hatami',
     phoneNumber: '+989396396040',
     birthDate: new Date(1981, 2, 28)
   }, {
-    email: 'jahensle85@me.com',
+    email: 'user2@example.com',
     firstName: 'Jared',
     lastName: 'Hensley',
     phoneNumber: '***',
     birthDate: new Date(1985, 7, 15)
   }, {
-    email: 'ctsongas@thinkful.com',
+    email: 'user3@example.com',
     firstName: 'Chris',
     lastName: 'Tsongas',
     phoneNumber: '***',
     birthDate: new Date(1991, 2, 8)
   }, {
-    email: 'mkhan@thinkful.com',
+    email: 'user4@example.com',
     firstName: 'Mohammad',
     lastName: 'Umair Khan',
     phoneNumber: '***',
     birthDate: new Date(1987, 9, 2)
   }, {
-    email: 'melanie.brgr@gmail.com',
+    email: 'user5@example.com',
     firstName: 'Michelle',
     lastName: 'Gerber',
     phoneNumber: '***',
     birthDate: new Date(1983, 3, 23)
   }, {
-    email: 'isha.ojha@gmail.com',
+    email: 'user6@example.com',
     firstName: 'Isha',
     lastName: 'Ojha',
     phoneNumber: '***',
     birthDate: new Date(1993, 11, 8)
   }, {
-    email: 'mtehrani@thinkful.com',
+    email: 'user7@example.com',
     firstName: 'Mahdi',
     lastName: 'Tehrani',
     phoneNumber: '***',
